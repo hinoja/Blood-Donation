a9f80c193767edc466f02a020710fd9ff2e91dfb
hinoja <hinoja2@gmail.com>
2023-03-26 09:52:54 +0200

    Mododication en cours

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,6 @@
+<?php return array (
+  'admin.contacts.contact-manage' => 'App\\Http\\Livewire\\Admin\\Contacts\\ContactManage',
+  'admin.users.user-manage' => 'App\\Http\\Livewire\\Admin\\Users\\UserManage',
+  'front.contact.contact-manage' => 'App\\Http\\Livewire\\Front\\Contact\\ContactManage',
+  'front.lang.change' => 'App\\Http\\Livewire\\Front\\Lang\\Change',
+);
