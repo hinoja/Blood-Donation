b42245c17e187d15e3ca2d09e66301e37935b57d
hinoja <hinoja2@gmail.com>
2023-05-30 09:04:40 +0200

    correcte form with login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-
 <!-- Mirrored from thememakker.com/templates/swift/hospital/locked.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 25 Feb 2023 08:01:25 GMT -->
 
 <head>
@@ -15,36 +14,48 @@
     <!-- Custom Css -->
     <link rel="stylesheet" href="{{ asset('assets/back/css/main.css') }}" />
 
+
 </head>
 
-<body  class="theme-cyan authentication">
+<body class="theme-cyan authentication">
+    <style>
+        .resize {
+            width: 50rem !important;
+            height: 40rem !important;
+            margin-left: -225px !important;
+            background-attachment: fixed;
+        }
+    </style>
     @include('sweetalert::alert')
     <div class="container">
-        <div class="card-top"></div>
-        <div class="card locked">
-            <h1 class="title"> @lang('Login')
-                {{-- <span class="msg">@lang('Sign In a  membership')</span> --}}
+        <!-- <div class="card-top"></div> -->
+        <div class="card resize locked ">
+            <h1 class="title" style="color: #ea3d3d;"> @lang('Login')
+                {{-- <span class="msg" >@lang('Sign In a  membership')</span> --}}
             </h1>
             <div class="d-flex">
                 <div class="thumb" style="margin:auto;">
-                    <a href="{{ route('home') }}"> <img class="media-object"
-                            src="{{ asset('assets/back/images/bg2.jpg') }}" class="rounded" alt="logo"></a>
+                    <a href="{{ route('home') }}">
+                        <img class="media-object" style="width: 150px !important;"
+                            src="{{ asset('assets/back/images/bg2.jpg') }}" class="rounded" alt="logo">
+                    </a>
                 </div>
-
             </div>
             <div class="body">
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
 
                     <!-- Email Address -->
-                    <div class="input-group icon before_span {{ $errors->has('email') ? 'has-error' : '' }}">
+                    <br>
+                    <div class="input-group icon before_span {{ $errors->has('email') ? 'has-error' : '' }}"
+                        style="width:500px; margin-left: 9rem !important;">
                         {{-- <label for="email" class="control-label">@lang('Email')</label> --}}
                         <span class="input-group-addon"> <i class="zmdi zmdi-email"></i> </span>
                         <div class="form-line">
                             <input id="email" type="email"
                                 class="form-control @error('email') is-invalid @enderror" name="email"
-                                placeholder="xyz@mail.com" value="{{ old('email') }}" tabindex="1" required
-                                autofocus>
+                                placeholder="xyz@mail.com" value="{{ old('email') }}" tabindex="1" required autofocus
+                                style="width: ;">
                             {{-- <input type="password" class="form-control" name="email" placeholder="Password" required> --}}
                         </div>
                         @error('email')
@@ -53,7 +64,9 @@
                     </div>
 
                     <!-- Password -->
-                    <div class="input-group icon before_span {{ $errors->has('password') ? 'has-error' : '' }}">
+                    <br>
+                    <div class="input-group icon before_span {{ $errors->has('password') ? 'has-error' : '' }}"
+                        style="width:500px; margin-left: 9rem !important;">
                         <span class="input-group-addon"> <i class="zmdi zmdi-lock"></i> </span>
                         <div class="form-line">
                             <input type="password" required autocomplete="current-password" class="form-control"
@@ -65,16 +78,15 @@
                     </div>
                     <br>
                     <!-- Remember Me -->
-                    <div>
+                    <div style="margin-left: 9rem !important;">
                         <input class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
                             name="remember" id="remember_me" type="checkbox" name="terms" id="terms"
                             class="filled-in chk-col-pink">
-                        <label for="remember_me"><span
-                                class="ml-2 text-sm text-gray-600">{{ __('Remember mjjke') }}</span>
+                        <label for="remember_me">
+                            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                         </label>
                     </div>
-
-                    {{-- <div class="block mt-4">
+                    {{-- <div class="block mt-4" ">
                         <label for="remember_me" class="inline-flex items-center">
                             <input id="remember_me" type="checkbox"
                                 class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
@@ -86,14 +98,14 @@
 
                         @if (Route::has('password.request'))
                             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                href="{{ route('password.request') }}">
+                                href="{{ route('password.request') }}" style="text-decoration: none;">
                                 {{ __('Forgot your password?') }}
                             </a>
                         @endif
                     </div>
-                    <br>
+                    <br><br>
                     <div class="text-center">
-                        <button type="submit" style="background: #ea3d3d;"
+                        <button type="submit" style="background: #ea3d3d; color: white;"
                             class="btn btn-raised waves-effect ">@lang('Login')</button>
                     </div>
                     {{-- <div class="text-center"><a href="sign-in.html">@lang('Sign In!')</a></div> --}}
@@ -101,67 +113,12 @@
             </div>
         </div>
     </div>
-
     {{-- <div class="theme-bg"></div> --}}
     <!-- Jquery Core Js -->
     <script src="{{ asset('assets/back/bundles/libscripts.bundle.js') }}"></script> <!-- Lib Scripts Plugin Js -->
     <script src="{{ asset('assets/back/bundles/vendorscripts.bundle.js') }}"></script> <!-- Lib Scripts Plugin Js -->
-
     <script src="{{ asset('assets/back/bundles/mainscripts.bundle.js') }}"></script><!-- Custom Js -->
 </body>
-
 <!-- Mirrored from thememakker.com/templates/swift/hospital/locked.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 25 Feb 2023 08:01:25 GMT -->
 
 </html>
-
-
-
-{{--
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox"
-                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout> --}}
